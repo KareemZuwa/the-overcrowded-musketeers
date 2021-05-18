a90507fc9a8277cda541c0bc84a96d15dbbfc444
elisabethdeborah <elisabeth_deborah@hotmail.com>
2021-05-18 09:33:25 +0200

    added html rendering

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 var setTimerForm = document.querySelector('#set-timer-form');
-var timeAmountText = document.getElementById('time-amount').innerHTML;
+var timeAmountText = document.getElementById('set-time-length');
 var timeAmount = 10;
 var increaseBtn = document.querySelector('#increase');
 var decreaseBtn = document.querySelector('#decrease');
 var intervalChecked = document.querySelector('#intervals-check');
 var breakChecked = document.querySelector('#break-check');
+var timeHeader = document.createElement('h1');
+timeHeader.innerText = "" + timeAmount;
+timeAmountText.insertBefore(timeHeader, increaseBtn);
 var increaseTime = function () {
     timeAmount += 1;
-    timeAmountText = "" + timeAmount;
-    console.log(timeAmount, timeAmountText);
+    timeHeader.innerText = "" + timeAmount;
 };
 var decreaseTime = function () {
     timeAmount -= 1;
-    timeAmountText = "" + timeAmount;
-    console.log(timeAmount, timeAmountText);
+    timeHeader.innerText = "" + timeAmount;
 };
 increaseBtn.onclick = function () { return increaseTime(); };
 decreaseBtn.onclick = function () { return decreaseTime(); };
 setTimerForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(intervalChecked.value, breakChecked.value, timeAmount);
+    console.log('Intervals: ', intervalChecked.checked, '5 min break between intervals: ', breakChecked.checked, 'Chosen time amount: ', timeAmount, 'Total interval and break time: ', timeAmount + 5);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
 
 const setTimerForm: HTMLFormElement = document.querySelector('#set-timer-form');
 
-let timeAmountText = document.getElementById('time-amount').innerHTML;
+let timeAmountText = document.getElementById('set-time-length');
 let timeAmount: number = 10;
 
-let increaseBtn: HTMLAnchorElement= document.querySelector('#increase')
-let decreaseBtn: HTMLAnchorElement = document.querySelector('#decrease')
-let intervalChecked: HTMLInputElement = document.querySelector('#intervals-check')
-let breakChecked: HTMLInputElement = document.querySelector('#break-check')
+let increaseBtn: HTMLAnchorElement= document.querySelector('#increase');
+let decreaseBtn: HTMLAnchorElement = document.querySelector('#decrease');
+let intervalChecked: HTMLInputElement = document.querySelector('#intervals-check');
+let breakChecked: HTMLInputElement = document.querySelector('#break-check');
 
-
+const timeHeader = document.createElement('h1');
+timeHeader.innerText = `${timeAmount}`;
+timeAmountText.insertBefore(timeHeader, increaseBtn);
 
 const increaseTime = () => {
-    timeAmount+=1
-    timeAmountText = `${timeAmount}`
-    console.log(timeAmount, timeAmountText);
+    timeAmount+=1;
+    timeHeader.innerText = `${timeAmount}`;
     
 };
 const decreaseTime = () => {
-    timeAmount-=1
-    timeAmountText = `${timeAmount}`
-    console.log(timeAmount, timeAmountText);
+    timeAmount-=1;
+    timeHeader.innerText = `${timeAmount}`;
 };
 
-increaseBtn.onclick = () => increaseTime()
-decreaseBtn.onclick = () => decreaseTime()
+increaseBtn.onclick = () => increaseTime();
+decreaseBtn.onclick = () => decreaseTime();
 
 setTimerForm.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-    console.log(intervalChecked.value, breakChecked.value, timeAmount);
+    console.log('Intervals: ', intervalChecked.checked, '5 min break between intervals: ', breakChecked.checked, 'Chosen time amount: ', timeAmount, 'Total interval and break time: ', timeAmount + 5);
     
 })
