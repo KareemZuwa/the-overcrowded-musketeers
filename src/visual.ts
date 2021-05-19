abff7511c8253588c97cff6979cd539c79b34638
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-05-19 12:15:04 +0200

    color update

--- src/visual.ts
+++ src/visual.ts
@@ -3,11 +3,12 @@ import { timeObject } from './index'
 const progress: any = document.querySelector('#pbar') //any sätts här för att få value att fungera med nummer.
 console.log(timeObject)
 
-function timeglass(){
 
-    let reverse_counter: number = 20; //värde från settimer ist för 20
+function timeglass(){
+    progress.max = 30;
+    let reverse_counter: number = 30; //värde från settimer ist för 20
     let downloadTimer: number = setInterval(function(){
-    progress.value = 20 - --reverse_counter; 
+    progress.value = 30 - --reverse_counter; 
     if(reverse_counter <= 0)
     clearInterval(downloadTimer);
     console.log(timeObject)
