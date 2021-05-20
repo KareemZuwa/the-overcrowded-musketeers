e99b040d8f92130ba348b5ea491436746a9964c2
NooraAli20 <noora_ali89@hotmail.com>
2021-05-20 14:29:59 +0200

    fixed progress bar return datatype in visuals.ts

--- src/visual.ts
+++ src/visual.ts
@@ -10,19 +10,19 @@ let test = () =>{
     timer.on('secondsUpdated', () =>{
     let minutes = timer.getTimeValues().minutes
     let seconds = timer.getTimeValues().seconds
-    let calculate: Number = minutes * 60 + seconds
+    let calculate: number = minutes * 60 + seconds
     console.log(calculate)
     El.innerHTML = '';
     El.insertAdjacentHTML('beforeend', digitalpause.render(timer));
 
-const progress: any = document.querySelector('#pbar') //any sätts här för att få value att fungera med nummer.
+const progress: HTMLProgressElement = document.querySelector('#pbar') //any sätts här för att få value att fungera med nummer.
 console.log(timeObject)
 
 function start_countdown(){
     console.log(calculate)
     
-    let reverse_counter: any = calculate; //värde från settimer ist för 20
-    let downloadTimer: any = setInterval(function(){
+    let reverse_counter: number = calculate; //värde från settimer ist för 20
+    let downloadTimer: number = setInterval(function(){
     progress.value = calculate - --reverse_counter; 
     if(reverse_counter <= 0)
     clearInterval(downloadTimer);
