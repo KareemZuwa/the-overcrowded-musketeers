e4ba3a264334bfb42985d4735dde05c86e02ce39
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-05-20 08:35:58 +0200

    starttimer branch working, progressbar not working

--- src/visual.ts
+++ src/visual.ts
@@ -1,67 +1,68 @@
-import { totalTimeInSeconds } from './index'
-import { Timer } from 'easytimer.js'
-import { startCountdown } from './TS-module/startcountdown'
-import * as digitalpause from './TS-module/digitalpause'
-
-let El: any = document.querySelector('#break-page')
-
-console.log(totalTimeInSeconds)
-const progress: any = document.querySelector('#pbar') //any sätts här för att få value att fungera med nummer.
-// let prog: HTMLElement = document.createElement('progress');
-//     prog.setAttribute('value', '20');
-//     prog.setAttribute('max', '20');
-//     document.getElementById('visual-page').appendChild(prog)
-
-function timing(){
-    console.log()
-    let reverse_counter: any = 20; //värde från settimer ist för 20
-    let downloadTimer: any = setInterval(function(){
-    progress.value = 20 - --reverse_counter; 
-    if(reverse_counter <= 0)
-    clearInterval(downloadTimer);
-},1000);
-}
-timing()
-export {timing}
-
-// import { timeObject } from './index'
+// import { totalTimeInSeconds } from './index'
 // import { Timer } from 'easytimer.js'
-// import { timer } from './TS-module/startcountdown'
+// import { startCountdown } from './TS-module/startcountdown'
 // import * as digitalpause from './TS-module/digitalpause'
 
 // let El: any = document.querySelector('#break-page')
 
-// let test = () =>{
+// console.log(totalTimeInSeconds)
+// const progress: any = document.querySelector('#pbar') //any sätts här för att få value att fungera med nummer.
+// // let prog: HTMLElement = document.createElement('progress');
+// //     prog.setAttribute('value', '20');
+// //     prog.setAttribute('max', '20');
+// //     document.getElementById('visual-page').appendChild(prog)
 
-// //     timer.on('secondsUpdated', () =>{
-// //     let minutes = timer.getTimeValues().minutes //får ut minuter & sekunder
-// //     let seconds = timer.getTimeValues().seconds
-// //     let calculate: Number = minutes * 60 + seconds //omvandlar för progress bar
-// //     console.log(calculate)
-// //     El.innerHTML = '';
-// //     El.insertAdjacentHTML('beforeend', digitalpause.render(timer));
-
-//  const progress: any = document.querySelector('#pbar') //any sätts här för att få value att fungera med nummer.
-// // console.log(calculate)
-
-
-// function start_countdown(calculate){
-//     console.log(calculate)
-    
+// function timing(){
+//     console.log()
 //     let reverse_counter: any = 20; //värde från settimer ist för 20
 //     let downloadTimer: any = setInterval(function(){
-//     progress.value = 20 - --reverse_counter;
+//     progress.value = 20 - --reverse_counter; 
 //     if(reverse_counter <= 0)
 //     clearInterval(downloadTimer);
-//     // console.log(timeObject)
 // },1000);
 // }
-// // start_countdown(calculate);
-// // })
-//  }
+// timing()
+// export {timing}
 
-// test()
-// export {test}
+ import { timeObject } from './index'
+ import { Timer } from 'easytimer.js'
+ import { timer } from './TS-module/startcountdown'
+ import * as digitalpause from './TS-module/digitalpause'
+
+ console.log(timeObject)
+ let El: any = document.querySelector('#break-page')
+
+ let test = () =>{
+
+     timer.on('secondsUpdated', () =>{
+     let minutes = timer.getTimeValues().minutes //får ut minuter & sekunder
+     let seconds = timer.getTimeValues().seconds
+     let calculate: Number = minutes * 60 + seconds + 1 //omvandlar för progress bar
+     console.log(calculate)
+     El.innerHTML = '';
+     El.insertAdjacentHTML('beforeend', digitalpause.render(timer));
+
+  const progress: any = document.querySelector('#pbar') //any sätts här för att få value att fungera med nummer.
+ console.log(calculate)
+
+let value: number = progress.value
+ function start_countdown(calculate){
+     console.log(calculate)
+    
+     let reverse_counter: any = calculate; //värde från settimer ist för 20
+     let downloadTimer: any = setInterval(function(){
+    value = calculate - --reverse_counter;
+     if(reverse_counter <= 0)
+     clearInterval(downloadTimer);
+ console.log(timeObject)
+ },1000);
+ }
+ start_countdown(calculate);
+ })
+  }
+
+ test()
+ export {test}
 
 
 
