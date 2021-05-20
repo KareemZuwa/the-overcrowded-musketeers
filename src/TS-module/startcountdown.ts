5fdb359642a1ac56f4eff4eda95166f8f91151b6
elisabethdeborah <elisabeth_deborah@hotmail.com>
2021-05-20 10:09:53 +0200

    digital timer

--- src/TS-module/startcountdown.ts
+++ src/TS-module/startcountdown.ts
@@ -2,12 +2,29 @@ import { Timer } from 'easytimer.js';
 
 let timer: Timer = new Timer();
 
+let digitalTimer: HTMLHeadingElement = document.querySelector('#digitalTime')
+
 // Start countdown on click, with times in seconds from form as arguments
 let startCountdown = (timeInSeconds: number, intervalOn: boolean, addBreak: boolean) => {
     timer.start({countdown: true, startValues: {seconds: timeInSeconds}, target: {seconds: 0}})
-    let countdownNumber = timeInSeconds;
+    let countdownNumber:number = timeInSeconds;
     timer.on('secondsUpdated', () => {
+       
         console.log('timer1 ', countdownNumber--);
+        console.log('modulus', countdownNumber % 60);
+        let seconds:number = countdownNumber % 60
+        let minutesFloat: any = countdownNumber / 60
+        let minutes:any = parseInt(minutesFloat)
+        if ( minutes < 10 && seconds < 10 ) {
+            digitalTimer.innerText = `0${minutes} : 0${seconds}`;
+        } else if ( minutes < 10 && seconds > 10 ) {
+            digitalTimer.innerText = `0${minutes} : ${seconds}`;
+        } else if ( minutes > 10 && seconds < 10 ) {
+            digitalTimer.innerText = `${minutes}: 0${seconds}`;
+        } else {
+            digitalTimer.innerText = `${minutes}: ${seconds}`;
+        }
+        
     })
     //If intervals is checked, restart timer after first interval ends, else stop timer
     timer.on('targetAchieved', () => {
